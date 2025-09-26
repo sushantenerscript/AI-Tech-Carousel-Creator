704d874c9f9581f2555b0ab6d0a256265d61ea6c
sushantenerscript <165885698+sushantenerscript@users.noreply.github.com>
2025-09-26 14:23:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Load env variables from .env files
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
     define: {
-      // Expose environment variables to the client-side code
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
     }
   };
