30583a437b339436c770d7a3570f83c8d9a71765
sushantenerscript <165885698+sushantenerscript@users.noreply.github.com>
2025-09-26 14:36:48 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -7,13 +7,3 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
   </React.StrictMode>,
 );
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(registration => {
-      console.log('SW registered: ', registration);
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError);
-    });
-  });
-}
